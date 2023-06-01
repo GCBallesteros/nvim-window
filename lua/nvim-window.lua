9149f18e49a41ec188fe1588dd05f7b2d1c5d21a
Guillem Ballesteros <guillem@maxwellrules.com>
2023-06-01 13:15:38 +0100

    Formatting

--- lua/nvim-window.lua
+++ lua/nvim-window.lua
@@ -13,218 +13,218 @@ local float_height = 3
 local float_width = 6
 
 local config = {
-  -- The characters available for hinting windows.
-  chars = {
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-  },
+    -- The characters available for hinting windows.
+    chars = {
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+    },
 
-  -- A group to use for overwriting the Normal highlight group in the floating
-  -- window. This can be used to change the background color.
-  normal_hl = 'Normal',
+    -- A group to use for overwriting the Normal highlight group in the floating
+    -- window. This can be used to change the background color.
+    normal_hl = 'Normal',
 
-  -- The highlight group to apply to the line that contains the hint characters.
-  -- This is used to make them stand out more.
-  hint_hl = 'Bold',
+    -- The highlight group to apply to the line that contains the hint characters.
+    -- This is used to make them stand out more.
+    hint_hl = 'Bold',
 
-  -- The border style to use for the floating window.
-  border = 'single',
+    -- The border style to use for the floating window.
+    border = 'single',
 }
 
 -- Returns a table that maps the hint keys to their corresponding windows.
 local function window_keys(windows)
-  local mapping = {}
-  local chars = config.chars
-  local nrs = {}
-  local ids = {}
-  local current = api.nvim_win_get_number(api.nvim_get_current_win())
+    local mapping = {}
+    local chars = config.chars
+    local nrs = {}
+    local ids = {}
+    local current = api.nvim_win_get_number(api.nvim_get_current_win())
 
-  -- We use the window number (not the ID) as these are more consistent. This in
-  -- turn should result in a more consistent choice of window keys.
-  for _, win in ipairs(windows) do
-    local nr = api.nvim_win_get_number(win)
+    -- We use the window number (not the ID) as these are more consistent. This in
+    -- turn should result in a more consistent choice of window keys.
+    for _, win in ipairs(windows) do
+        local nr = api.nvim_win_get_number(win)
 
-    table.insert(nrs, nr)
-    ids[nr] = win
-  end
-
-  table.sort(nrs)
-
-  local index = 1
-
-  for _, nr in ipairs(nrs) do
-    -- We skip the current window here so that we still "reserve" it the
-    -- character, but don't include it in the output. This ensures that window X
-    -- always gets hint Y, regardless of what the current active window is.
-    if nr ~= current then
-      local key = chars[index]
-
-      if mapping[key] then
-        key = key .. (index == #chars and chars[1] or chars[index + 1])
-      end
-
-      mapping[key] = ids[nr]
+        table.insert(nrs, nr)
+        ids[nr] = win
     end
 
-    index = index == #chars and 1 or index + 1
-  end
+    table.sort(nrs)
 
-  return { mapping, index - 1 }
+    local index = 1
+
+    for _, nr in ipairs(nrs) do
+        -- We skip the current window here so that we still "reserve" it the
+        -- character, but don't include it in the output. This ensures that window X
+        -- always gets hint Y, regardless of what the current active window is.
+        if nr ~= current then
+            local key = chars[index]
+
+            if mapping[key] then
+                key = key .. (index == #chars and chars[1] or chars[index + 1])
+            end
+
+            mapping[key] = ids[nr]
+        end
+
+        index = index == #chars and 1 or index + 1
+    end
+
+    return { mapping, index - 1 }
 end
 
 -- Opens all the floating windows in (roughly) the middle of every window.
 local function open_floats(mapping)
-  local floats = {}
+    local floats = {}
 
-  for key, window in pairs(mapping) do
-    local bufnr = api.nvim_create_buf(false, true)
+    for key, window in pairs(mapping) do
+        local bufnr = api.nvim_create_buf(false, true)
 
-    if bufnr > 0 then
-      local win_width = api.nvim_win_get_width(window)
-      local win_height = api.nvim_win_get_height(window)
+        if bufnr > 0 then
+            local win_width = api.nvim_win_get_width(window)
+            local win_height = api.nvim_win_get_height(window)
 
-      local row = math.max(0, math.floor((win_height / 2) - 1))
-      local col = math.max(0, math.floor((win_width / 2) - float_width))
+            local row = math.max(0, math.floor((win_height / 2) - 1))
+            local col = math.max(0, math.floor((win_width / 2) - float_width))
 
-      api.nvim_buf_set_lines(
-        bufnr,
-        0,
-        -1,
-        true,
-        { '', '  ' .. key .. '  ', '' }
-      )
-      api.nvim_buf_add_highlight(bufnr, 0, config.hint_hl, 1, 0, -1)
+            api.nvim_buf_set_lines(
+                bufnr,
+                0,
+                -1,
+                true,
+                { '', '  ' .. key .. '  ', '' }
+            )
+            api.nvim_buf_add_highlight(bufnr, 0, config.hint_hl, 1, 0, -1)
 
-      local float_window = api.nvim_open_win(bufnr, false, {
-        relative = 'win',
-        win = window,
-        row = row,
-        col = col,
-        width = #key == 1 and float_width - 1 or float_width,
-        height = float_height,
-        focusable = false,
-        style = 'minimal',
-        border = config.border,
-        noautocmd = true,
-      })
+            local float_window = api.nvim_open_win(bufnr, false, {
+                relative = 'win',
+                win = window,
+                row = row,
+                col = col,
+                width = #key == 1 and float_width - 1 or float_width,
+                height = float_height,
+                focusable = false,
+                style = 'minimal',
+                border = config.border,
+                noautocmd = true,
+            })
 
-      api.nvim_win_set_option(
-        float_window,
-        'winhl',
-        'Normal:' .. config.normal_hl
-      )
-      api.nvim_win_set_option(float_window, 'diff', false)
+            api.nvim_win_set_option(
+                float_window,
+                'winhl',
+                'Normal:' .. config.normal_hl
+            )
+            api.nvim_win_set_option(float_window, 'diff', false)
 
-      floats[float_window] = bufnr
+            floats[float_window] = bufnr
+        end
     end
-  end
 
-  -- We need to redraw here, otherwise the floats won't show up
-  vim.cmd 'redraw'
+    -- We need to redraw here, otherwise the floats won't show up
+    vim.cmd 'redraw'
 
-  return floats
+    return floats
 end
 
 local function close_floats(floats)
-  for window, bufnr in pairs(floats) do
-    api.nvim_win_close(window, true)
-    api.nvim_buf_delete(bufnr, { force = true })
-  end
+    for window, bufnr in pairs(floats) do
+        api.nvim_win_close(window, true)
+        api.nvim_buf_delete(bufnr, { force = true })
+    end
 end
 
 local function get_char()
-  local ok, char = pcall(fn.getchar)
+    local ok, char = pcall(fn.getchar)
 
-  return ok and fn.nr2char(char) or nil
+    return ok and fn.nr2char(char) or nil
 end
 
 -- Configures the plugin by merging the given settings into the default ones.
 function M.setup(user_config)
-  config = vim.tbl_extend('force', config, user_config)
+    config = vim.tbl_extend('force', config, user_config)
 end
 
 -- Picks a window to jump to, and makes it the active window.
 function M.pick()
-  local windows = vim.tbl_filter(function(id)
-    return api.nvim_win_get_config(id).relative == ''
-  end, api.nvim_tabpage_list_wins(0))
+    local windows = vim.tbl_filter(function(id)
+        return api.nvim_win_get_config(id).relative == ''
+    end, api.nvim_tabpage_list_wins(0))
 
-  local _aux = window_keys(windows)
-  local window_keys = _aux[1]
-  local n_windows = _aux[2]
+    local _aux = window_keys(windows)
+    local window_keys = _aux[1]
+    local n_windows = _aux[2]
 
-  if n_windows == 2 then
-    for _, win in pairs(window_keys) do
-      api.nvim_set_current_win(win)
-        return nil
+    if n_windows == 2 then
+        for _, win in pairs(window_keys) do
+            api.nvim_set_current_win(win)
+            return nil
+        end
     end
-  end
 
-  local floats = open_floats(window_keys)
-  local key = get_char()
-  -- local window = nil
+    local floats = open_floats(window_keys)
+    local key = get_char()
+    -- local window = nil
 
-  if not key or key == escape then
+    if not key or key == escape then
+        close_floats(floats)
+        return
+    end
+
+    local window = window_keys[key]
+    local extra = {}
+    local choices = 0
+
+    for hint, win in pairs(window_keys) do
+        if vim.startswith(hint, key) then
+            extra[hint] = win
+            choices = choices + 1
+        end
+    end
+
+    if choices > 1 then
+        close_floats(floats)
+
+        floats = open_floats(extra)
+
+        local second = get_char()
+
+        if second then
+            local combined = key .. second
+
+            window = window_keys[combined] or window_keys[key]
+        else
+            window = nil
+        end
+    end
+
     close_floats(floats)
-    return
-  end
 
-  local window = window_keys[key]
-  local extra = {}
-  local choices = 0
-
-  for hint, win in pairs(window_keys) do
-    if vim.startswith(hint, key) then
-      extra[hint] = win
-      choices = choices + 1
+    if window then
+        api.nvim_set_current_win(window)
     end
-  end
-
-  if choices > 1 then
-    close_floats(floats)
-
-    floats = open_floats(extra)
-
-    local second = get_char()
-
-    if second then
-      local combined = key .. second
-
-      window = window_keys[combined] or window_keys[key]
-    else
-      window = nil
-    end
-  end
-
-  close_floats(floats)
-
-  if window then
-    api.nvim_set_current_win(window)
-  end
 end
 
 return M
